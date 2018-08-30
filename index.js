180fd40f4767791ebd99f8b1fc011e61242e3bb7
Cherie Lum <charismatic.cherie@gmail.com>
2018-08-30 16:28:44 -0400

    Adding 5 total cats

--- index.js
+++ index.js
@@ -8,7 +8,37 @@
 // };
 
 var i = 0;
+var x =0;
+var y =0;
+var z =0;
+var a =0;
+
 function buttonClick() {
     i++;
-    document.getElementById('number').value = i;
+    //document.getElementById('number1').value = i;
+    document.getElementById('number1').innerText = i;
+}
+
+function buttonClick2() {
+    x++;
+    //document.getElementById('number2').value = x;
+    document.getElementById('number2').innerText = x;
+}
+
+function buttonClick3() {
+    y++;
+    //document.getElementById('number2').value = x;
+    document.getElementById('number3').innerText = y;
+}
+
+function buttonClick4() {
+    z++;
+    //document.getElementById('number2').value = x;
+    document.getElementById('number4').innerText = z;
+}
+
+function buttonClick5() {
+    a++;
+    //document.getElementById('number2').value = x;
+    document.getElementById('number5').innerText = a;
 }
